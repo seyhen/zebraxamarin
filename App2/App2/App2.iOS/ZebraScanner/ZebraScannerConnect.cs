de4e3f38dd01c8f62ddd1259c9d390c2a62f3772
Ridhwan Romjon <ridhwan.romjon@retail247.com>
2019-11-22 17:24:17 +0000

    more messing around

--- App2/App2/App2.iOS/ZebraScanner/ZebraScannerConnect.cs
+++ App2/App2/App2.iOS/ZebraScanner/ZebraScannerConnect.cs
@@ -19,13 +19,83 @@ namespace App2.iOS.ZebraScanner
         }
         public List<string> GetScannerList()
         {
-            var m = ScannerExtensions.GetAvailableScanners(SbtSdkApi);
-         
+            SbtSdkApi = (SbtSdkApiWrapper)Zebra.SbtSdkFactory.CreateInstance();
+            SbtSdkApi.SbtSetDelegate(new MyDelegate());
+            
+            var bob = SbtSdkApi.EnableAvailableScannersDetection(true);
+            
+            var availableReaders = new NSMutableArray();
+            var availableHandle = availableReaders.Handle;
+            
+            var n = SbtSdkApi.GetAvailableScannersList(ref availableHandle);
+
+            var scanners = NSArray.ArrayFromHandle<ScannerInfo>(availableHandle);
+
+
+            var availableReaders2 = new NSMutableArray();
+            var availableHandle2 = availableReaders2.Handle;
+
+            var m = SbtSdkApi.GetActiveScannersList(ref availableHandle2);
+
+
+            var scanners2 = NSArray.ArrayFromHandle<ScannerInfo>(availableHandle2);
+
             List<string> result = new List<string>();
+
+
+            SbtSdkApi.EstablishCommunicationSession(0);
 
             return result;
         }
 
 
+    }
+
+    public class MyDelegate : SbtSdkApiDelegate
+    {
+        public override void EventBarcode(string barcodeData, BarcodeType barcodeType, int scannerID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void EventBarcodeData(NSData barcodeData, BarcodeType barcodeType, int scannerID)
+        {
+            Console.WriteLine("Scanned a biiii: ");
+        }
+
+        public override void EventCommunicationSessionEstablished(ScannerInfo activeScanner)
+        {
+            Console.WriteLine("Connected");
+        }
+
+        public override void EventCommunicationSessionTerminated(int scannerID)
+        {
+            Console.WriteLine("Lost connection");
+        }
+
+        public override void EventFirmwareUpdate(FirmwareUpdateEvent fwUpdateEventObj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void EventImage(NSData imageData, int scannerID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void EventScannerAppeared(ScannerInfo availableScanner)
+        {
+            Console.WriteLine("Found mofo scanner");
+        }
+
+        public override void EventScannerDisappeared(int scannerID)
+        {
+            Console.WriteLine("Lost mofo scanner");
+        }
+
+        public override void EventVideo(NSData videoFrame, int scannerID)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
